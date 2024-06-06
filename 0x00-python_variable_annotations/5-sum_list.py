7afc403b14de9b300fd683d937c01362c934fa2c
Darknught <hassino66@yahoo.com>
2024-06-06 14:54:22 +0000

    code

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" A Module that defines a type annotated function sum_list."""
+
+
+def sum_list(input_list: float) -> float:
+    """ Takes in a list of floats and returns their sum."""
+    return sum(input_list)
