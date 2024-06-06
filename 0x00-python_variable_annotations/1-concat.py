4c5ac4db02dbe4f7fd6edd5f4bcd03cca5ba8b61
Darknught <hassino66@yahoo.com>
2024-06-06 14:02:12 +0000

    code

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Module that defines a function concat """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ Takes in 2 args and returns a concatenated
+    string.
+    """
+    return str1 + str2
