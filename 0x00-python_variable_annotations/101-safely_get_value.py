d86c55e9c9d38b0f65d188834174a6d59754ce7b
Darknught <hassino66@yahoo.com>
2024-06-06 20:31:23 +0000

    code

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """ Function that defines type annotations."""
-from typing import TypeVar, Mapping, Any, Optional
+from typing import TypeVar, Mapping, Any, Union
 T = TypeVar('T')
 
 
 def safely_get_value(
-        dct: Mapping[Any, T], key: Any, default: Optional[T] = None
-        ) -> Optional[T]:
+        dct: Mapping[Any, T], key: Any, default: Union[T, None] = None
+        ) -> Union[Any, T]:
     """ Functions handle dictionaries qith any type of keys and values
     Providing the correct type hints for the return value based on the input
     dictionary and default value.
@@ -14,7 +14,7 @@ def safely_get_value(
         dct - Mapping[Any, T] indicates dct is a mapping with keys of any type
               and values of Type T
         Key - Any indicates the key can be of any type
-        default - Optional[T] = None specifies that default value can be of
+        default - Union[T, None] = None specifies that default value can be of
                   type T or none
     """
     if key in dct:
