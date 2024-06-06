0d38f7e42fb35cfc8df542e57dd6e26174631229
Darknught <hassino66@yahoo.com>
2024-06-06 15:36:16 +0000

    code

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """ A module that annotate a functions parameters."""
-from typing import List, Tuple
+from typing import Iterable, Sequence, List, Tuple
 
 
-def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """ takes in one argument lst which is a list of strings and
     returns the list of tuples where each tuple contaiuns an int
     """
