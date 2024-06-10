0d1fe30479cdf883fa331b379a596f10f566365f
Darknught <hassino66@yahoo.com>
2024-06-10 14:47:08 +0000

    correcting the dynamic import in task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -4,19 +4,19 @@ import asyncio
 import importlib.util
 from typing import List
 
-# Dynamic import of wait_random from 0-basic_async_syntax.py
-spec = importlib.util.spec_from_file_location(
-        "basic_async_syntax", "0-basic_async_syntax.py")
-basic_async_syntax = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(basic_async_syntax)
-wait_random = basic_async_syntax.wait_random
-
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """ Takes in two int args and spawn wait_random n times with a specified
     max_delay.
     Returns a list of all the delays(float) and should be in ascending order
     """
+    # Dynamic import of wait_random from basic_async_syntax.py
+    spec = importlib.util.spec_from_file_location(
+            "basic_async_syntax", "0-basic_async_syntax.py")
+    basic_async_syntax = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(basic_async_syntax)
+    wait_random = basic_async_syntax.wait_random
+
     tasks = [wait_random(max_delay) for _ in range(n)]
     delays = await asyncio.gather(*tasks)
 
