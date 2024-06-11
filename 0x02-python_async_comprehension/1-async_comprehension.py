324de0de7d295800ec6b7b0b0cf10e2328e60380
Darknught <hassino66@yahoo.com>
2024-06-11 14:10:25 +0000

    Importing async generator then writing an async comprehension

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Module that imports async_generator and write another coroutine."""
+import asyncio
+import random
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension():
+    """The coroutine collects 10 random numbers using async comprehension
+    over async_generator then returns 10 random numbers.
+    """
+    result = [i async for i in async_generator()]
+    return result
