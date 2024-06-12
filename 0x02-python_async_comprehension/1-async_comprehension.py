3cc1fca8f5ac0b46f8312bca1150cab82285f170
Darknught <hassino66@yahoo.com>
2024-06-12 12:28:52 +0000

    Forgot to indicate the return value type

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,10 +2,11 @@
 """ Module that imports async_generator and write another coroutine."""
 import asyncio
 import random
+from typing import Generator, List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """The coroutine collects 10 random numbers using async comprehension
     over async_generator then returns 10 random numbers.
     """
