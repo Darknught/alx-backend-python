fcc079a96751d737e9cfd0def1bf8adc6caaf9ad
Darknught <hassino66@yahoo.com>
2024-06-12 12:31:02 +0000

    Forgot to indicate the return value type

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -7,7 +7,7 @@ import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime():
+async def measure_runtime() -> float:
     """ The function measures the runtime of executing async_comprehension
     four times in parallel.
     """
