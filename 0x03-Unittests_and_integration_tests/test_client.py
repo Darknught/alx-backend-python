1016305299fcd208c183262089a567ab5109c889
Darknught <hassino66@yahoo.com>
2024-06-29 17:19:06 +0000

    code

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Module to Test the GithubOrgClient class"""
 import unittest
-from unittest.mock import patch, PropertyMock
+from unittest.mock import patch, PropertyMock, Mock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -106,6 +106,23 @@ class TestGithubOrgClient(unittest.TestCase):
             # assert that get_json was called once with expected url
             mock_get_json.assert_called_once_with(
                     "https://api.github.com/orgs/test_org/repos")
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+        ({}, "my_license", False)
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """
+        Test the has_license method of GithubOrgClient.
+
+        Parameters:
+        repo (dict): The repository data.
+        license_key (str): The license key to check.
+        expected (bool): The expected result.
+        """
+        result = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(result, expected)
 
 
 if __name__ == "__main__":
